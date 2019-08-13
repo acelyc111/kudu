7bc8d41a10439d94673f2f86f2d1b490c7c43ed0
Yingchun Lai <405403881@qq.com>
2019-08-13 10:32:55 +0800

    [thirdparty] Bump LZ4 to version 1.9.1
    New version of LZ4 have some speed improvements, we ought to
    upgrade it.
    
    Change-Id: I11c0de2db6d524b7f40c905a2a0abb0cb12a281b

--- thirdparty/download-thirdparty.sh
+++ thirdparty/download-thirdparty.sh
@@ -330,12 +330,11 @@ fetch_and_patch \
   "patch -d projects/compiler-rt -p1 < $TP_DIR/patches/llvm-tsan-disable-trace-switching-after-multithreaded-for.patch" \
   "patch -d projects -p1 < $TP_DIR/patches/llvm-ustat-removal.patch"
 
-LZ4_PATCHLEVEL=1
+LZ4_PATCHLEVEL=0
 fetch_and_patch \
- lz4-lz4-$LZ4_VERSION.tar.gz \
+ lz4-$LZ4_VERSION.tar.gz \
  $LZ4_SOURCE \
- $LZ4_PATCHLEVEL \
- "patch -p1 < $TP_DIR/patches/lz4-0001-Fix-cmake-build-to-use-gnu-flags-on-clang.patch"
+ $LZ4_PATCHLEVEL
 
 BITSHUFFLE_PATCHLEVEL=0
 fetch_and_patch \
